a66520a40f7b52a607bce6e5ae785eb62d415e7f
Ahmed Abdulrahman <a.kasapbashi@live.com>
2018-09-11 01:06:31 +0200

    :sparkles: Add Edit File function, and some changes in the controller

--- app/Http/Controllers/FilesController.php
+++ app/Http/Controllers/FilesController.php
@@ -12,17 +12,33 @@ class FilesController extends Controller
     private $extensionsList = ['jpg', 'png', 'gif', 'jpeg'];
 
     /**
-     * Get All Files
+     * Display a listing of the Files.
+     *
+     * @param Request $request
+     * @return Response
      */
     public function index()
     {
         $files = Files::all();
-        return view('files')->with('files', $files);
+        return view('files.index')->with('files', $files);
 
     }
 
     /**
-     * Store Files
+     * Show the form for creating a new File.
+     *
+     * @return Response
+     */
+    public function create()
+    {
+        return view('files.create');
+    }
+
+    /**
+     * Store a newly created File in storage.
+     *
+     * @param Request $request
+     * @return Response
      */
     public function store(Request $request)
     {
@@ -33,20 +49,106 @@ class FilesController extends Controller
             $file = $request->file('photo');
             // Extract all info needed for storing ... ;)
             $extension = $file->getClientOriginalExtension();
-            $name = basename($file->getClientOriginalName(), "." . $extension);
+            $name = preg_replace("/[\s_]/", "-", basename($file->getClientOriginalName(), "." . $extension));
             $fileType = in_array($extension, $this->extensionsList) ? 'Images' : 'Others';
             $fileSize = $file->getClientSize();
 
             if (Storage::putFileAs('/public/' . $fileType, $file, $name. '.' . $extension)) {
-                return $model::create([
+                $file =  $model::create([
                     'name'      => $name,
                     'type'      => $fileType,
                     "extension" => $extension,
                     "size"      => $fileSize
                 ]);
+                return redirect(route('files.index'));
             }
-
-            return back();
         }
+    }
+
+    /**
+     * Display the specified File.
+     *
+     * @param  int $id
+     *
+     * @return Response
+     */
+    public function show($id)
+    {
+
+    }
+
+    /**
+     * Show the form for editing the specified File.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function edit($id)
+    {
+        $file = Files::findOrFail($id);
+        return view('files.edit')->with('file', $file);
+    }
+
+    /**
+     * Update the specified File in storage.
+     *
+     * @param  int              $id
+     * @param Request $request
+     * @return Response
+     */
+    public function update($id, Request $request)
+    {
+        // Create an instance of Files
+        $model = new Files();
+
+        //Find find by ID
+        $file = $model::where('id', $id)->first();
+
+        // Get Old file location
+        $oldFile = '/public/'. $file->type . '/'. $file->name . '.' . $file->extension;
+
+        // Check Request for File
+        if ($request->hasFile('photo')) {
+
+            // Get the uploaded file
+            $uploadedFile = $request->file('photo');
+
+            // Extract all info needed for storing ... ;)
+            $extension = $uploadedFile->getClientOriginalExtension();
+            $name      = preg_replace("/[\s_]/", "-", basename($uploadedFile->getClientOriginalName(), "." . $extension));
+            $fileType  = in_array($extension, $this->extensionsList) ? 'Images': 'Others';
+            $fileSize  = $uploadedFile->getClientSize();
+
+            // Create location for new uploaded file
+            $newFile = '/public/' . $fileType . '/' . $name . '.' . $extension;
+
+            // Time to replace old file with new file
+            // 1- Check if exists
+            if (Storage::disk('local')->exists($oldFile)) {
+                 // 2- Update old file with new one ;)
+                if (Storage::disk('local')->move($oldFile, $newFile)) {
+                    // 3- Store file information into DB
+                    $file->name      = $name;
+                    $file->type      = $fileType;
+                    $file->extension = $extension;
+                    $file->size      = $extension;
+                    $file->save();
+
+                    // Redirect back to index view
+                    return redirect(route('files.index'));
+                }
+            }
+        }
+    }
+
+   /**
+     * Remove the specified File from storage.
+     *
+     * @param  int $id
+     * @return Response
+     */
+    public function destroy($id)
+    {
+
     }
 }
